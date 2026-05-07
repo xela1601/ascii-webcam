f4556ba8bb3cc9bd370d59441073dbb8cca2ec96
Xela (OpenCode) <opencode@alexander-schreiner.de>
2026-05-07 16:48:20 +0200

    add black & white mode toggle (-b flag, b key)

--- src/render.rs
+++ src/render.rs
@@ -99,7 +99,11 @@ pub fn render(
 
                 let (r, g, b) = (raw[idx], raw[idx + 1], raw[idx + 2]);
                 let ch = app.lut[grayscale(r, g, b) as usize];
-                ansi.push_str(&format!("\x1b[38;2;{r};{g};{b}m{ch}"));
+                if app.bw {
+                    ansi.push_str(&format!("\x1b[38;2;255;255;255m{ch}"));
+                } else {
+                    ansi.push_str(&format!("\x1b[38;2;{r};{g};{b}m{ch}"));
+                }
                 app.last_frame_plain.push(ch);
             }
         }
@@ -109,14 +113,15 @@ pub fn render(
     let mirror_label = if app.mirror { "M" } else { " " };
     let invert_label = if app.invert { "I" } else { " " };
     let transp_label = if transposed { "T" } else { " " };
+    let bw_label = if app.bw { "B" } else { " " };
     let cam_label = format!("{orig_w}x{orig_h}");
 
     let fps_text = format!(" {fps:3} FPS ");
     let hud = format!(
         "\x1b[{hud_row};1H\x1b[48;2;30;30;30m\x1b[38;2;180;180;180m\
          {fps_text}\
-         ┃ {mirror_label}{invert_label}{transp_label} ┃ {cam_label} -> {render_w}x{render_h} \
-         ┃ q:quit  r:inv  m:mir  t:trn  c:capture\
+         ┃ {mirror_label}{invert_label}{transp_label}{bw_label} ┃ {cam_label} -> {render_w}x{render_h} \
+         ┃ q:quit  r:inv  m:mir  t:trn  b:bw  c:capture\
          \x1b[K\x1b[0m",
         hud_row = term_h,
     );
